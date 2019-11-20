75e491c5b91c8c96d9c3623eb32825954f97dc90
Rozanna <rozanna.boczar@gmail.com>
2019-11-20 21:47:05 +0100

    yare yare

--- Projects/IO/src/Controller.java
+++ Projects/IO/src/Controller.java
@@ -5,7 +5,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
 //jeszcze potrzeba - pobrac nazwe XD, koszty dostawy,ocene
 //CENE SPRAWDZAĆ W PASKU WYSZUKIWAŃ BO JAK BEDZIE SIE WCHODZIĆ W STRONĘ TO ZAJMIE CIUT WIECEJ NIZ 15 SEKUND XD
 // warunki! czyli zakres cen, minalna ilosć opini 50
@@ -16,24 +15,26 @@ import java.util.ArrayList;
 
 //wynik nazwa produktu wraz z linkiem
 
+
 public class Controller {
 
     //Skąpiec.pl
-    private ArrayList<Product> products = new ArrayList<>(); //lista produktow
-    private ArrayList<String> links = new ArrayList<>(); //lista linków do kupienia
+    private Product[] products = new Product[5];
+    private Result[][] results = new Result[5][3];
 
+    // bez Product product, bedzie korzystac z tablicy, niech wyrzuca tablice wynikow
     public void Search(Product product) throws IOException {
 
-        Connection connect = Jsoup.connect("https://www.skapiec.pl/szukaj/w_calym_serwisie/"+product.Get_Name()+"/price");
-        Document document ;
-        Elements search_site ;//strony z wynikami wyszukiwania
+        Connection connect = Jsoup.connect("https://www.skapiec.pl/szukaj/w_calym_serwisie/" + product.Get_Name() + "/price");
+        Document document;
+        Elements search_site;//strony z wynikami wyszukiwania
         do {
             document = connect.get();//łączenie - strona z wyszukiwaniami
             // trzeba jeszcze opcje gdy pojawi się 1 produkt - bo wtedy nic nie znajduje XDD
             search_site = document.select("a.pager-btn.arrow.right");
-            Elements start_price = document.select("a.box"); //strona z produktem na Skapiec
+            //Elements start_price = document.select("a.box"); //strona z produktem na Skapiec
             Elements more_info = document.select("a.more-info"); //strona z produktem na Skapiec
-            Elements compare_link = document.select("a.compare-link-1"); //strona z wieloma produktami w jednym sklepie
+            Elements compare_link = document.select("a.compare-link-1"); //strona z produktem w wielu sklepach
 
 
             for (Element alem : more_info) {
@@ -43,38 +44,48 @@ public class Controller {
                 Elements price = document.select("span.price.gtm_or_price");
                 Elements shipping = document.select("a.delivery-cost.link.gtm_oa_shipping");
                 Elements link = document.select("a.offer-row-item.gtm_or_row");
-/*
-                for (Element eh : name )
-                {
-                    System.out.println("Name: " + eh.text());
-                }
-*/
-                for (Element eh : price)
-                {
-                    Double double_price = Double.parseDouble(eh.text().substring(0,eh.text().length()-2).replace(",","."));
-                    if(double_price<product.get_Range()[1] && double_price>product.get_Range()[0] ){
+                Elements nr_opinions = document.select("span.counter"); //liczba opinii sklepu
+                Elements opinion = document.select("span.counter"); //opinia sklepu hmmm gwiazdki....
 
-                        for (Element eh2 : name )
-                        {
-                            System.out.println("Name: " + eh2.text());
+                for (Element eh : price) {
+                    String url_link = "";
+                    Double double_price = Double.parseDouble(eh.text().substring(0, eh.text().length() - 2).replace(",", "."));
+                    if (double_price < product.get_Range()[1] && double_price > product.get_Range()[0]) {
+
+                        if (Integer.parseInt(nr_opinions.text())>= 50) { //ograniczenie na liczbę opinii sklepu
+                            //if (Integer.parseInt(eh1.text()) >= 50) {
+
+                                for (Element eh2 : name) {
+                                    System.out.println("Name: " + eh2.text());
+                                }
+
+                                System.out.println("Cena:" + double_price);
+
+                                for (Element eh3 : shipping) { //po kij shipping XD
+                                    System.out.println(eh3.text());//tutaj trzeba link i wziąć przesyłkę itd
+                                }
+
+                                for (Element eh4 : link) {
+                                    //działa XD
+                                    // System.out.println("https://www.skapiec.pl"+eh4.attr("href"));//link do produktu w sklepie
+                                    url_link = "https://www.skapiec.pl" + eh4.attr("href");
+
+                                }
+
+                                for (int i = 0; i < 3; i++) {
+                                    if (results[1][i] != null) {
+                                        results[1][i] = new Result(product.Get_Name(), url_link);
+                                        System.out.println(results[1][i].getLink());
+                                    }
+                                }
+
+                            //}
+
                         }
-
-                        System.out.println("Cena:" + double_price);
-
-                        for (Element eh3 : shipping )
-                        {
-                            System.out.println(eh3.text());//tutaj trzeba link i wziąć przesyłkę itd
-                        }
-
-                        for (Element eh4 : link )
-                        {
-                            //działa XD
-                           // System.out.println("https://www.skapiec.pl"+eh4.attr("href"));//link do produktu w sklepie
-
-                        }
-
                     }
                 }
+            }
+
                 /*
                 for (Element eh : shipping )
                 {
@@ -85,7 +96,7 @@ public class Controller {
                     System.out.println(eh.text());//link do produktu w sklepie
                 }
 */
-            }
+
             /*
             for (Element alem : compare_link) {
                 connect = Jsoup.connect("https://www.skapiec.pl" + alem.attr("href"));
@@ -107,24 +118,26 @@ public class Controller {
                 }
 
 
+                */
 
-            }
-                 */
+
             for (Element elem : search_site) {
                 connect = Jsoup.connect("https://www.skapiec.pl" + elem.attr("href"));
             }
-
-        }while(search_site.size()==1);
+        }
+                while (search_site.size() == 1) ;
 
     }
 
+        public static void main (String[]args) throws IOException {
+            Double[] range = new Double[2];
+            range[0] = 10.0;
+            range[1] = 40.0;
+            Product product2 = new Product("lalka h", 5, range, 4.5);
+            //dodac produkty do tablicy
+            // w search zrobić pętle po tablicy
+            Controller controller = new Controller();
+            controller.Search(product2);
 
-    public static void main(String[] args) throws IOException {
-        Double[] range = new Double[2];
-        range[0] = 10.0;
-        range[1] = 40.0;
-        Product product = new Product("lalka h", 5, range, 4.5);
-       Controller controller = new Controller();
-       controller.Search(product);
+        }
     }
-}
